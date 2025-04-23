7edff0e240f1e3366513180751ac19a6ebdfb43d
mgisrael393823 <misrael00@gmail.com>
2025-04-23 09:38:58 -0500

    chore: stub Project Management API endpoints with mock data

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
 import express from 'express';
+import { MOCK_PROJECTS } from './lib/mockData';
 const app = express();
 
 // -- API route stubs for integration tests --
 app.get('/creators', (_req, res) => res.json([]));
 app.get('/creators/:id', (_req, res) => res.sendStatus(404));
 
-app.get('/projects', (_req, res) => res.json([]));
-app.get('/projects/:id', (_req, res) => res.sendStatus(404));
+app.get('/projects', (_req, res) => {
+  res.json(MOCK_PROJECTS);
+});
+
+app.get('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const project = MOCK_PROJECTS.find(p => p.id === id);
+  if (project) {
+    res.json(project);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 app.get('/applications', (_req, res) => res.json([]));
 app.get('/applications/:id', (_req, res) => res.sendStatus(404));
